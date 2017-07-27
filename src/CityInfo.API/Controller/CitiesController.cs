16e3b7c2adedc56a1562cd4b35f50a6dd398e404
Arnaud Couchet <aco@softfluent.com>
2017-07-27 17:49:27 +0200

    Get api
    Get the list of cities

---
+++ src/CityInfo.API/Controller/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controller
+{
+    public class CitiesController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "name"},
+                new { id = 2, Name = "name2"}
+            });
+        }
+    }
+}
