7cfaa118658f639d2413a631e8aa6da7023a0e8e
Arnaud Couchet <aco@softfluent.com>
2017-08-09 17:25:52 +0200

    Restaurent update.

---
+++ src/CityInfo.API/Entities/OdeToFoodDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CityInfo.API.Entities
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Restaurents> Restaurants { get; set; }
+    }
+}
